4a2838302c8dd1a4b83736849ed98d963c7cb484
Yebincho03 <lucycho0314@gmail.com>
2022-11-06 17:05:23 -0800

    add project section

---
+++ portfolio-web/components/Project.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Image from "next/image";
+import styled from "styled-components";
+
+const Project = (props) => {
+  const { featureTitle, featureHeadline, featureDescription, featureImg } =
+    props;
+
+  return <></>;
+};
+
+export default Project;
---
+++ portfolio-web/components/ProjectsContainer.js
@@ -0,0 +1,39 @@
+import Project from "./Project";
+import styled from "styled-components";
+
+const ProjectsContainer = () => {
+  return (
+    <Container id="projects">
+      <Project
+        featureTitle="Explore"
+        featureHeadline="Find tennis courts and check their availability"
+        featureDescription="Save time by planning to visit parks with the shortest wait time and have easy access to all available court options in your community."
+        featureImg="/images/image-explore.png"
+      />
+      <Project
+        featureTitle="Play"
+        featureHeadline="Join or queue at a court and track your play time"
+        featureDescription="Simplify the use and sharing of courts by organizing players in a queue system—and don't lose sight of your place in line with notifications!"
+        featureImg="/images/image-play.png"
+      />
+      <Project
+        featureTitle="Match"
+        featureHeadline="Set your preferences and pair up with a player"
+        featureDescription="Cut down on communication and find other users' looking for someone to play with based on your preferences."
+        featureImg="/images/image-match.png"
+      />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 0;
+  margin: 0;
+  margin-top: 13.7vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default ProjectsContainer;
