2b9efabe8642b7bb5dca9a92c065f1da295f94c8
Yebincho03 <lucycho0314@gmail.com>
2022-11-21 18:43:13 -0800

    change header

--- portfolio-web/components/Header.js
+++ portfolio-web/components/Header.js
@@ -60,12 +60,7 @@ const Menu = styled.div`
   margin: 0 5px;
 
   .contactMe {
-    line-height: 40px;
-    margin: 0 10px;
-    font-size: 25px;
-    font-weight: 500;
-    cursor: default;
-    visibility: hidden;
+    opacity: 0;
 
     @media screen and (min-width: 800px) {
       line-height: 40px;
@@ -73,6 +68,7 @@ const Menu = styled.div`
       font-size: 20px;
       font-weight: 500;
       cursor: default;
+      opacity: 1;
     }
   }
 `;
